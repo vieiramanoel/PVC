3eceee01da4616efc7d62d192be1a0ff6485bc75
Vieira Neto <vieiranetoc@gmail.com>
2016-04-12 20:36:48 -0300

    New files for PD_2, first part implemented already, take two points and calculate distance between them

---
+++ PD_1/Codigo_2.x/CMakeFiles/PD1_UsingMAT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/PD1_UsingMAT.dir/PD1_UsingMAT.cpp.o"
+  "PD1_UsingMAT.pdb"
+  "PD1_UsingMAT"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/PD1_UsingMAT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ PD_2/PointDistance/src/Main.cpp
@@ -0,0 +1,9 @@
+#include "PointClickDistance.hpp"
+
+int main(int argc, char** argv){
+    PointClickDistance windowClick;
+
+    windowClick.gettingROI();
+    windowClick.getDistance();
+    return 0;
+}
