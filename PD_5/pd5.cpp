fb7b297072e8f94b815bccefc29f0c4fe5c17672
Vieira <vieiranetoc@gmail.com>
2016-05-19 00:13:30 -0300

    finished pd5

--- PD_5/pd5.cpp
+++ PD_5/pd5.cpp
@@ -3,11 +3,12 @@
 
 void initializeGLCM(int* glcmMat);
 void normalizeGLCM(cv::Mat glcmMat);
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cout << "invalid number of arguments, returning" << std::endl;
+        std::cout << "invalid number of arguments" << std::endl << "please pass only one image as argument, returning" << std::endl;
         return -1;
     }
 
@@ -39,11 +40,10 @@ int main(int argc, char *argv[])
 
     initializeGLCM((int*)glcmMat);
     cv::Mat glcm45(256,256, CV_32FC1);
-
     for (int i = 1; i < inputImg.rows; ++i)
-        for (int j = 1; j < inputImg.cols - 1; ++j)
-           glcmMat[(int)inputImg.at<uchar>(i-1, j+1)][(int)inputImg.at<uchar>(i-1, j+1)]++;
-
+        for (int j = 0; j < inputImg.cols - 1; ++j)
+           glcmMat[(int)inputImg.at<uchar>(i, j)][(int)inputImg.at<uchar>(i-1, j+1)]++;
+        
     for (int i = 0; i < 256; ++i)
         for (int j = 0; j < 256; ++j)
             glcm45.at<float>(i,j) = (float)glcmMat[i][j];
@@ -70,18 +70,13 @@ void normalizeGLCM(cv::Mat glcm){
     {
         p = glcm.ptr<float>(i);
         for (int j = 0; j < 256; ++j)
-        {
             sum += p[j];
-        }
     }
 
     for (int i = 0; i < 256; ++i)
     {
         p = glcm.ptr<float>(i);
         for (int j = 0; j < 256; ++j)
-        {
-            p[j] /= sum
-;
-        }
+            p[j] /= sum;
     }
 }
